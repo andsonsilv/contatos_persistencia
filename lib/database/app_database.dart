f23fee43cde4dd1151b114d14a44409454985e09
Andson <andsonsilva@alu.ufc.br>
2021-10-29 10:30:40 -0300

    Renomeação .db

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -3,7 +3,7 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 Future<Database> getDatabase() async {
-  final String path = join(await getDatabasesPath(), 'database.db');
+  final String path = join(await getDatabasesPath(), 'database_contatos.db');
   return openDatabase(
     path,
     onCreate: (db, version) {

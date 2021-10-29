8025a7202f10227461c294d27a34af59e8f99278
Andson <andsonsilva@alu.ufc.br>
2021-10-29 09:13:40 -0300

    Config database

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,14 +1,14 @@
+import 'package:contatos_persistencia/database/dao/contato_dao.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-void createDatabase(){
-  getDatabasesPath().then((dbPath) {
-    final String path = join(dbPath, 'mybank.db');
-    openDatabase(path, onCreate: (db, version){
-      db.execute('CREATE TABLE contacts('
-          'id INTEGER PRIMARY KEY, '
-          'name TEXT, '
-          'account_number INTEGER)');
-    }, version: 1);
-  });
+Future<Database> getDatabase() async {
+  final String path = join(await getDatabasesPath(), 'database.db');
+  return openDatabase(
+    path,
+    onCreate: (db, version) {
+      db.execute(ContatoDao.tableSql);
+    },
+    version: 1,
+  );
 }
